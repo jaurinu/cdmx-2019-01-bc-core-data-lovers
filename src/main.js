a10d2228d0656e9bb026459e58288d19a0d816b9
jaurinu <jaurinu@gmail.com>
2019-02-24 11:51:57 -0600

    Se corrige/cambia en HTML la clase por id de las páginas. Se habilitan botones. Marca error Unchecked runtime.lastError ='( pero si carga la data ='(

--- src/main.js
+++ src/main.js
@@ -1,21 +1,59 @@
 
-//Constantes del DOM//
+//Constantes de páginas//
 const initialPage = document.getElementById('initial-page');
-const vehiclePage = document.getElementById('vehicle-page');
+const motoPage = document.getElementById('moto-page');
+const bicyclePage = document.getElementById('bicycle-page');
+const carPage = document.getElementById('car-page');
+const pedestrianPage = document.getElementById('pedestrian-page');
 const yearPage = document.getElementById('year-page');
 const allYearPage = document.getElementById('all-year-page');
 
 //Constantes botones página inicial//
+const motorcycleButton = document.getElementById('motorcycle-button');
+const bicycleButton = document.getElementById('bicycle-button');
+const carButton = document.getElementById('car-button');
+const pedestrianButton = document.getElementById('pedestrian-button');
+const oneYearButton = document.getElementById('one-year-button');
+const allYearButton = document.getElementById('all-year-button');
 
-
-
+//Constantes páginas//
 
 const printData = document.getElementById('print-data');
 const injuriesData='./data/injuries/injuries.json';
 const printMotorcyclists=document.getElementById('print-motorcyclists');
 
-//Cambios de página//
+//Eventos de páginas//
+motorcycleButton.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    motoPage.classList.remove('hidePage');
+});
 
+bicycleButton.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    bicyclePage.classList.remove('hidePage');
+});
+
+carButton.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    carPage.classList.remove('hidePage');
+});
+
+pedestrianButton.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    pedestrianPage.classList.remove('hidePage');
+});
+
+oneYearButton.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    yearPage.classList.remove('hidePage');
+});
+
+allYearButton.addEventListener('click', ()=>{
+    initialPage.classList.add('hidePage');
+    allYearPage.classList.remove('hidePage');
+});
+
+//Fetch//
 
 fetch(injuriesData)
 .then((response)=>{
