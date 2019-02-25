b763ff8b53862dd78e1308af9eefc30167050095
jaurinu <jaurinu@gmail.com>
2019-02-24 23:11:12 -0600

    Función que reconoce el botón seleccionado y función de filtrado por indicador de heridos de motocicleta. Intentaré usar forEach

--- src/data.js
+++ src/data.js
@@ -1,6 +1,61 @@
 window.injuries = {
 
-indicatorInjuries: (responseJson)=>{
+justFiveYears: (responseJson)=>{
+  console.log(responseJson)
+const minimumYear=('2010-01-04')
+const maximumYear=('2016-01-04')
+const justFiveYears=responseJson.filter(element=>element.Year>=(minimumYear)&&element.Year<(maximumYear))
+//console.log(justFiveYears);
+return justFiveYears;
+
+},
+
+indicatorInjuries: (justFiveYears)=>{
+  
+  // let chosenButton=motorcycleButton;
+  // if (chosenButton=motorcycleButton){
+    // const arrMot=[];
+
+
+    //-----------------funciona pero intentare solo con for each------------//
+    //-------------filtra por total injured persons motorcyclists-------------//
+    justFiveYears.filter(element=>{
+      const totalInjuriesByMoto=element.Total_Injured_Persons_Motorcyclists
+      console.log(totalInjuriesByMoto);
+      return totalInjuriesByMoto;
+    });
+    //-------------------por que retorna lista?--------------------------//
+//-------------------------fin de filtrado por total heridos moto-------------//
+//-----------------------------------------------------------------------//
+      // const year=element.Year;
+      // arrMot.push({motorcyclists, year});
+
+    
+  // }
+  
+  },
+
+  // indicatorInjuries: (justFiveYears, chosenButton)=>{
+  //   console.log(justFiveYears);
+  //   const arrMot=[];
+  //   justFiveYears.forEach(element => {
+  //     let motorcyclists= element.Total_Injured_Persons_Motorcyclists;
+  //     let pedalcyclists= element.Total_Injured_Persons_Pedalcyclists;
+  //     let carOccupant= element.Total_Injured_Persons_Passenger_Or_Occupant;
+  //     let Pedestrians= element.Total_Injured_Persons_Pedestrians;
+  //     let year=element.Year;
+  //     arrMot.push({motorcyclists, year});
+  //   });
+  //   return arrMot;
+  // }
+
+
+
+
+
+};
+
+
   // const arrFiveYears=[];
 //   responseJson.filter(element=>{
 // //     let year=element.year;
@@ -20,33 +75,3 @@ indicatorInjuries: (responseJson)=>{
 // const justFiveYears=arrYears.filter(year=>year>=('2013-01-04'));
 // console.log(justFiveYears);
 //------------------------probando filtro que traiga todo el elemento--------//
-
-
-const minimumYear=('2013-01-04')
-
-const justFiveYears=responseJson.filter(element=>element.Year>=(minimumYear))
-
-console.log(justFiveYears);
-
-
-
-
-},
-
-
-  // indicatorInjuries: (responseJson)=>{
-  //   console.log(responseJson);
-  //   const arrMot=[];
-  //   responseJson.forEach(element => {
-  //     let motorcyclists= element.Total_Injured_Persons_Motorcyclists;
-  //     let year=element.Year;
-  //     arrMot.push({motorcyclists, year});
-  //   });
-  //   return arrMot;
-  // }
-
-
-
-
-
-};
--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ const carButton = document.getElementById('car-button');
 const pedestrianButton = document.getElementById('pedestrian-button');
 const oneYearButton = document.getElementById('one-year-button');
 const allYearButton = document.getElementById('all-year-button');
+const hexagonButton = document.getElementsByClassName('hexagon-button');
 
 //Constante fetch data//
 const injuriesData='./data/injuries/injuries.json';
@@ -55,33 +56,59 @@ allYearButton.addEventListener('click', ()=>{
 });
 
 //Fetch//
-
+const data=window.injuries
 fetch(injuriesData)
 .then((response)=>{
 console.log(response);
 return response.json();
 })
-// .then(responseJson=>window.injuries.justFiveYears(responseJson))
-.then(responseJson=>{
-    console.log(responseJson);
+.then(responseJson=>window.injuries.justFiveYears(responseJson))
+.then(justFiveYears => {console.log(justFiveYears);
+return window.injuries.indicatorInjuries(justFiveYears)})
+
+
+
+// .then(responseJson=>{
+//     console.log(responseJson);
     
-    return window.injuries.indicatorInjuries(responseJson)
-})
+//     return window.injuries.indicatorInjuries(responseJson)
+// })
 .then(arrMotResp => printMot(arrMotResp))
 
 .catch((error)=>{
     console.info('hubo un problema '+error.message);
 });
 
+// const chosenButton=hexagonButton.addEventListener('click',()=>{
+//     hexagonButton.forEach(element=>{
+//         console.log(element)
+//     (element.id)
+//     console.log(element.id);
+//     });
+// })
+//--------------por que for each no es una funcion?----------//
+// const chosenButton='';
+// chosenButton=hexagonButton.forEach(elemento=> {
+//     console.log(elemento)
+//     let idbutton=elemento.id;
+//     console.log(idbutton);
+// });
 
-
-const printMot=(arrMotResp) =>{
-    arrMotResp.forEach(element => {
-        const printP=`<p>Año: ${element.year} - <span>${element.motorcyclists}</span> </p>`
-        printMotorcyclists.insertAdjacentHTML('beforeend', printP)
-    });
+//--------------este for si funciona----------//
+// for (let i=0; i<hexagonButton.length; i++){
+//     hexagonButton[i].addEventListener('click',()=>{
+//         const chosenButton=hexagonButton[i].id;
+//         console.log(chosenButton);
+//     })
+// }   
+//--------------imprime -----------------//
+// const printMot=(arrMotResp) =>{
+//     arrMotResp.forEach(element => {
+//         const printP=`<p>Año: ${element.year} - <span>${element.motorcyclists}</span> </p>`
+//         printMotorcyclists.insertAdjacentHTML('beforeend', printP)
+//     });
      
-    }
+//     }
 
 
 
